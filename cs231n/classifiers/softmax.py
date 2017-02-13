0ffa7fdf9c3ead2d5c017f65e342ff0e41bb2664
JoshZastrow <j.a.zastrow.jr@gmail.com>
2017-02-13 21:21:18 +0900

    Get Softmax to run correctly

--- cs231n/classifiers/softmax.py
+++ cs231n/classifiers/softmax.py
@@ -1,6 +1,58 @@
 import numpy as np
 
 
+def softmax_loss_naive(W, X, y, reg):
+    """
+    Softmax loss function, naive implementation (with loops)
+    Inputs:
+    - W: C x D array of weights (ex: 10 x 3073 features)
+    - X: D x N array of data. Data are D-dimensional columns
+    - y: 1-dimensional array of length N with labels 0...K-1, for K classes
+    - reg: (float) regularization strength
+    Returns:
+    a tuple of:
+    - loss as single float
+    - gradient with respect to weights W, an array of same size as W
+    """
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
+
+    for i in range(X.shape[1]):  # for each example
+        # create the linear hypthothesis for each class
+        scores = W.dot(X[:, i])
+        scores -= np.max(scores)  # prevents numerical instability
+        prob = 0.0  # initialize a probability for scores
+        loss -= scores[y[i]]  # subtract the correct score from the loss
+
+        for curr_score in scores:  # for each class
+            # sum up the scores to get a total probability
+            prob += np.exp(curr_score)
+
+        for j in range(W.shape[0]):  # for each class
+            # get the probability of this class score being correct
+            prob_ji = np.exp(scores[j]) / prob
+            # gradient descent step, probability multiplied by Xi
+            margin = - prob_ji * X[:, i].T
+
+            if j == y[i]:  # if this class is the correct one
+                # penalize for any difference in probability not 100%
+                margin = (1 - prob_ji) * X[:, i].T
+                # place the gradient in the gradient matrix
+                dW[j, :] += -margin
+
+        loss += np.log(prob)
+
+    loss /= X.shape[1]
+    dW /= X.shape[1]
+
+    # Regularization
+    loss += 0.5 * reg * np.sum(W * W)
+    dW += reg * W
+
+    return loss, dW
+
+
 def softmax_loss_vectorized(W, X, y, reg):
     """
     Softmax loss function, vectorized
@@ -18,40 +70,42 @@ def softmax_loss_vectorized(W, X, y, reg):
     # Initialize weights, loss, get dataset size
     dW = np.zeros_like(W)
     loss = 0.0
-    N = y.shape[0]
+    # Parameters
+    N = X.shape[1]  # Training Examples
+    # D = X.shape[0]  # Dimensions/Features
+    # K = W.shape[0]  # Classes
 
     # Perform Softmax
     #################################################################
+    # print('\n\nSample set, {} classes {} example(s):\n '.format(K, N))
+    # print('Weights:\n{}\n\n'.format(W[:K, :N]))
+    # print('Examples:\n{}\n\n'.format(X[:D, :N]))
+    # print('Labels:\n{}\n\n'.format(y[:N]))
 
     # Generate a linear classifier scores
     scores = W.dot(X)
 
-    # Normalize the exponents -- highest value should be zero
-    # This is to preserve numerical stablity
-    C = -np.amax(scores)
-
-    # Softmax on the scores:
-    scores = C * np.exp(scores)
+    # Normalize, so max score is 0 per class
+    scores -= np.amax(scores, axis=0).reshape((1, N))
 
     # Sum the scores for the loss function
-    sum_scores = np.sum(scores, axis=0)
+    prob = np.exp(scores) / np.exp(scores).sum(axis=0, keepdims=True)
+    # print('Softmax applied to scores:\n{}'.format(prob[:, :]))
 
-    # Get the correct class scores
-    y_predictions = scores[y, np.arange(N)]
+    # Get the probabilities for the correct class label
+    prob_y = prob[y, np.arange(N)]
+    # print('\nCorrect Score probability:\n{}'.format(prob_y[:]))
 
-    # Loss Function
-    loss = (y_predictions / sum_scores)  # score / total score
-    loss = -np.log(loss)
-    loss = np.sum(loss)
-    loss += reg * np.sum(scores**2)
+    # Cross Entropy Loss Function
+    loss = 1 / N * np.sum(-np.log(prob_y)) + 0.5 * reg * np.sum(W**2)
 
-    # Gradient Descent
-    probablilities = - scores / sum_scores  # K X N
-    probablilities[y, np.arange(N)] += 1
+    # print('Loss value: ', loss)
+    prob[y, np.arange(N)] += 1
 
-    dW = np.dot(probablilities, X.T)  # K X N * N X D = K X D
-    dW /= - X.shape[1]
-    dW = reg * W
+    # Gradient,
+    dW = np.dot(prob, X.T)  # K X N * N X D = K X D
+    dW /= -N  # divide by N examples
+    dW += reg * W
 
     return loss, dW
 
@@ -87,15 +141,20 @@ def numerical_gradient(f, x):
 
     return grad
 
-# Weights
-M = 15
-D = 6
-K = 3
 
-# Weights
-W = np.random.random_integers(1, 5, size=(K, D))
-X = np.random.random_integers(1, 6, size=(D, M))
-y = np.random.random_integers(0, K - 1, size=(M))
-r = 5
+def test_softmax():
 
-softmax_loss_vectorized(W, X, y, r)
+    # Parameters
+    N = 1  # Training Example size (of 1)
+    D = 30  # Dimensions/Features
+    K = 5  # Classes
+
+    # Weights
+    W = np.random.random(size=(K, D))
+    X = np.random.random_integers(1, 255, size=(D, N))
+    y = np.random.random_integers(0, K - 1, size=(N))
+    r = 5
+
+    softmax_loss_vectorized(W, X, y, r)
+
+# test_softmax()
--- cs231n/classifiers/softmax_example.py
+++ cs231n/classifiers/softmax_example.py
@@ -19,16 +19,31 @@ def softmax_loss_naive(W, X, y, reg):
     loss = 0.0
     dW = np.zeros_like(W)
 
-    ##########################################################################
-    # TODO: Compute the softmax loss and its gradient using explicit loops.     #
-    # Store the loss in loss and the gradient in dW. If you are not careful     #
-    # here, it is easy to run into numeric instability. Don't forget the        #
-    # regularization!                                                           #
-    ##########################################################################
+    for i in range(X.shape[1]):
+        scores = W.dot(X[:, i])
+        scores -= np.max(scores)  # prevents numerical instability
+        prob = 0.0
+        loss -= scores[y[i]]
 
-    ##########################################################################
-    #                          END OF YOUR CODE                                 #
-    ##########################################################################
+        for curr_score in scores:
+            prob += np.exp(curr_score)
+
+        for j in range(W.shape[0]):
+            prob_ji = np.exp(scores[j]) / prob
+            margin = - prob_ji * X[:, i].T
+
+            if j == y[i]:
+                margin = (1 - prob_ji) * X[:, i].T
+                dW[j, :] += -margin
+
+        loss += np.log(prob)
+
+    loss /= X.shape[1]
+    dW /= X.shape[1]
+
+    # Regularization
+    loss += 0.5 * reg * np.sum(W * W)
+    dW += reg * W
 
     return loss, dW
 
